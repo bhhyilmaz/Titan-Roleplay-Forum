7a3e3b8714356eb2f8288067b04ac47ae8f02098
Blainegith <Blainegith@github.com>
2024-01-06 04:26:54 +0300

    [Update] update server.

--- server.js
+++ server.js
@@ -21,73 +21,7 @@ const session = require('express-session');
 const cors = require('cors')
 require('dotenv').config();
 mongoDb();
-const { createAdapter } = require("@socket.io/mongo-adapter");
-const { MongoClient } = require("mongodb");
-const { Emitter } = require("@socket.io/mongo-emitter");
 //
-
-// Socket.io
-const PM = require('./models/PM');
-
-const io = require('socket.io')(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    credentials: true
-  }
-})
-
-
-// MongoDB adapter && emitter
-const DB = "forum_titan-roleplay";
-const COLLECTION = "pms";
-const mongoClient = new MongoClient(process.env.DB_URL);
-
-mongoClient.connect();
-const mongoCollection = mongoClient.db(DB).collection(COLLECTION);
-
-mongoCollection.createIndex(
-  { createdAt: 1 },
-  { expireAfterSeconds: 3600, background: true }
-);
-
-io.adapter(createAdapter(mongoCollection, {
-  addCreatedAtField: true
-}));
-
-const emitter = new Emitter(mongoCollection);
-
-// Socket.io main
-io.on('connection', (socket) => {
-  socket.on('sendMessage', (data) => {
-    if (data.username) {
-      try {
-        PM.create({
-          author: data.username,
-          receiver: data.receiver,
-          content: data.message
-        });
-      } catch (error) {
-        console.log(error);
-      };
-    };
-  });
-
-  socket.on('currentChatInfo', (data) => {
-    setTimeout(() => { // Timeout for the wait the real results for that the client re-render two times the data
-      try {
-        PM.find({
-          author: data.username,
-          receiver: data.receiver
-        }).then(db => {
-          emitter.emit('chatInfo', db);
-        })
-      } catch (error) {
-        console.log(error)
-      };
-    }, 250)
-  });
-});
 
 // Options
 const corsOptions = {
